704212fc418d1e495db49a4122308aa6fd8ca90a
Nestor <nestor.venegas95@gmail.com>
2021-03-19 21:18:33 -0500

    commit tree 2f380f0bcb1cb60765299b50b8838042e07bcd59

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.destroy_all
+
+c1 = Coupon.save(coupon_code: "sdioughsiud", store: "hello")
+c2 = Coupon.savecoupon_code: "s6fg45df5+d", store: "hello")
+c3 = Coupon.save(coupon_code: "6+5849fdh+9", store: "oof")
